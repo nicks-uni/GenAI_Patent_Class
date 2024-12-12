ccaa62b7db79eb8d105d9e5b29c342dbb0371edc
Gerrit Kruse <gerrit@kruse-netz.de>
2024-12-12 11:14:57 +0100

    Updated path in sbatch file

--- inference.sh
+++ inference.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --array=1-2
+#SBATCH --array=0-1
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --partition=gpu2080
@@ -11,7 +11,6 @@
 
 #SBATCH --job-name=XLNet-Classification
 #SBATCH --output=logs/%x-%A-partition_%a.out
-#SBATCH --error=logs/%x-%A-partition_%a.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=g_krus03@uni-muenster.de
 
@@ -20,6 +19,6 @@ module load palma/2024a
 module load GCCcore/13.3.0
 module load Python/3.12.3
 
-INPUT_FILE="nick_data/data/20240823_Applications_Grants_Combined/partition_${SLURM_ARRAY_TASK_ID}.csv"
+INPUT_FILE="data/20240823_Applications_Grants_Combined/partition_${SLURM_ARRAY_TASK_ID}.csv"
 
 srun venv/bin/python train_transf_class_newdata.py --input-file "$INPUT_FILE" --partition-number ${SLURM_ARRAY_TASK_ID}
